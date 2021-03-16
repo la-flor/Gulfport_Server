969587529421c07f435a6411bd636af128047eb6
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-16 09:22:44 -0600

    Changed size requirements in forms

--- forms.py
+++ forms.py
@@ -4,14 +4,14 @@ from wtforms.validators import InputRequired, Length, DataRequired, Email, Equal
 from models import User
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Length(min=6, max=25)])
+    email = StringField('Email', validators=[InputRequired(), Length(min=6, max=50)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=25)])
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+                        validators=[DataRequired(), Email(), Length(min=6, max=50)])
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8, max=25)])
     confirm_password = PasswordField("Confirm Password",
                                         validators=[DataRequired(), EqualTo("password")])
